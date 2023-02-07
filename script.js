4f9bb1368cbcaa03b793c57c410066cfc7adcabe
Glutey <mr_tscreen@hotmail.com>
2023-02-07 17:41:42 +0000

    Added api calls, functions and CSS code for search

--- script.js
+++ script.js
@@ -1,41 +1,121 @@
-var apiKey = d00e97b068c768695d109b4ec943d226;
+var apiKey = 'd00e97b068c768695d109b4ec943d226';
 //api key for use on the OpenWeather site
 
-var queryUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
-// for the 5 day forcast
+$(document).ready(
+  $("#search-button").click(function(event){
+    event.preventDefault();
+  var cityName = $("#search-input").val();
+  var queryUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
+  var currentForecast = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
+  console.log(cityName);
 
-var geoCode = 'http:/ /api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
-//direct geocoding api for places by name and not lat and long
 
+  $.ajax({
+    url: currentForecast,
+    method: "GET",
+    statusCode: {
+      404 : function() {
+        alert ("Re-show input. City not found.");
+      },
+      200 : function(response){
+        console.log(currentForecast);
+        console.log(response);
+        for (var i = 0; i < response.list.length; i += 4){
+          console.log(response.list[i]);
+          function currentSave(){
+            var dayOne = document.getElementById
+
+          }
+          // store into local storeage here
+        }
+      }
+    }
+  })
 // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
-    // url: queryURL,
-    url: geoCode,
-    method: "GET"
+    url: queryUrl,
+    method: "GET",
+    statusCode: {
+      404 : function() {
+        alert ("Re-show input. City not found.");
+      },
+      200 : function(response){
+        console.log(queryUrl);
+        console.log(response);
+        
+        for (var i = 0; i < response.list.length; i += 8){
+          // jquery newcard for each day containing all in info such as 
+          console.log(response.list[i]);
+          // store into local storage here
+        }
+      }
+    }
   })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
-  
-      // Log the queryURL
-      console.log(queryURL);
-  
-      // Log the resulting object
-      console.log(response);
   
       // Transfer content to HTML
-    //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    //   $(".wind").text("Wind Speed: " + response.wind.speed);
-    //   $(".humidity").text("Humidity: " + response.main.humidity);
+      // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      // $(".wind").text("Wind Speed: " + response.wind.speed);
+      // $(".humidity").text("Humidity: " + response.main.humidity);
       
-      // Convert the temp to Celsius
-    //   var tempC = response.main.temp - 273.15;
+      // // Convert the temp to Celsius
+      // var tempC = response.main.temp - 273.15;
   
-      // add temp content to html
-    //   $(".temp").text("Temperature (K) " + response.main.temp);
-    //   $(".tempC").text("Temperature (C) " + tempC.toFixed(2));
+      // // add temp content to html
+      // $(".temp").text("Temperature (K) " + response.main.temp);
+      // $(".tempC").text("Temperature (C) " + tempC.toFixed(2));
   
       // Log the data in the console as well
     //   console.log("Wind Speed: " + response.wind.speed);
     //   console.log("Humidity: " + response.main.humidity);
     //   console.log("Temperature (C): " + tempC);
+  }));
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+    var appID = "d00e97b068c768695d109b4ec943d226";
+
+    $(".query_btn").click(function(){
+
+        var query_param = $(this).prev().val();
+
+        if ($(this).prev().attr("placeholder") == "City") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
+        } else if ($(this).prev().attr("placeholder") == "Zip Code") {
+            var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
+        }
+
+        $.getJSON(weather,function(json){
+            $("#city").html(json.name);
+            $("#main_weather").html(json.weather[0].main);
+            $("#description_weather").html(json.weather[0].description);
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#temperature").html(json.main.temp);
+            $("#pressure").html(json.main.pressure);
+            $("#humidity").html(json.main.humidity);
+        });
+    })
+
+    // Optional Code for temperature conversion
+    var fahrenheit = true;
+
+    $("#convertToCelsius").click(function() {
+        if (fahrenheit) {
+            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+        }
+        fahrenheit = false;
     });
+
+    $("#convertToFahrenheit").click(function() {
+        if (fahrenheit == false) {
+            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
+        }
+        fahrenheit = true;
+    });
+});
