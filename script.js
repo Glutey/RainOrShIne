d00730ec0025b435ad89e78d478b7c212e7c0116
Glutey <mr_tscreen@hotmail.com>
2023-02-07 17:55:36 +0000

    current weather api was wrong now corrected

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ $(document).ready(
   $("#search-button").click(function(event){
     event.preventDefault();
   var cityName = $("#search-input").val();
+  var weatherToday = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=appid=d00e97b068c768695d109b4ec943d226';
   var queryUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
-  var currentForecast = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
+//   var currentForecast = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
   console.log(cityName);
 
 
   $.ajax({
-    url: currentForecast,
+    url: weatherToday, // changed the api call as the original was wrong.
     method: "GET",
     statusCode: {
       404 : function() {
         alert ("Re-show input. City not found.");
       },
       200 : function(response){
-        console.log(currentForecast);
+        console.log(weatherToday);
         console.log(response);
         for (var i = 0; i < response.length; i += 4){ //removed the .list from this line as it isnt applicable to the object being called
           console.log(response.length[i]);
