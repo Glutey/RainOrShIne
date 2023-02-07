ac3a81eedf27957ddb8b51b6b29c2c06179a9d0c
Glutey <mr_tscreen@hotmail.com>
2023-02-07 20:59:34 +0000

    dded info to update the HTML and present findings

--- README.md
+++ README.md
@@ -31,12 +31,12 @@ SO THAT I can plan a trip accordingly
 * Create a weather dashboard with form inputs.
   * When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
   * When a user views the current weather conditions for that city they are presented with:
-    * The city name
+    *' The city name
     * The date
     * An icon representation of weather conditions
     * The temperature
     * The humidity
-    * The wind speed
+    * The wind speed'
   * When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
     * The date
     * An icon representation of weather conditions
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(
   $("#search-button").click(function(event){
     event.preventDefault();
   var cityName = $("#search-input").val();
-  var weatherToday = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=appid=d00e97b068c768695d109b4ec943d226';
+  var weatherToday = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
   var queryUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
 //   var currentForecast = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=d00e97b068c768695d109b4ec943d226';
   console.log(cityName);
@@ -22,22 +22,32 @@ $(document).ready(
         var currentDate = moment().now;
         console.log(weatherToday);
         console.log(response);
-        for (var i = 0; i < response.length; i++){ //removed the .list from this line as it isnt applicable to the object being called
-          console.log(response.length[i]);
-          var jsonString = JSON.stringify(response);
-          localStorage.setItem("weatherData", jsonString);
-          console.log(currentDate);
-        }
+        //removed the .list from this line as it isnt applicable to the object being called
+        console.log(response.length);
+        var jsonString = JSON.stringify(response);
+        localStorage.setItem("weatherData", jsonString);
+        console.log(currentDate);
+          
+          // Transfer content to HTML
+        $("#city").text('City: ' + response.name);
+        $("#main_weather").html('Outlook today ' + response.weather[0].main);
+        $("#description_weather").html('Description ' + response.weather[0].description);
+        $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+        $("#temperature").html('Temperature ' + response.main.temp);
+        $("#pressure").html('Pressure ' + response.main.pressure);
+        $("#humidity").html('Humidity ' + response.main.humidity);
       }
     }
   })
-// Here we run our AJAX call to the OpenWeatherMap API
+// wrap this in an event listener that actions when the user clicks the button for the current days weather, it will then populate that 
+// location with the forecast for the next 5 days
+// including: date, temperature, humidity and weather icon
 $.ajax({
     url: queryUrl,
     method: "GET",
     statusCode: {
       404 : function() {
-        alert ("Re-show input. City not found.");
+        alert ("City not found.");
       },
       200 : function(response){
         console.log(queryUrl);
@@ -47,22 +57,28 @@ $.ajax({
           // jquery newcard for each day containing all in info such as 
           console.log(response.list[i]);
           // store into local storage here
+          
+          //   $("#today").html("<h1>" + response.name + " Weather Details</h1>");
+    //   $('#today').text('Date: ' + date);
+    //   $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+    //   $("#today").text("Wind Speed: " + response.wind.speed);
+      
+    //   $('#today').text('Temperature: ' + );
+    //   $(".humidity").text("Humidity: " + response.main.humidity);
+      
         }
       }
     }
   })
   
-      // Transfer content to HTML
-      $("#today").html("<h1>" + response.name + " Weather Details</h1>");
-      The city name
-      * The date
-      * An icon representation of weather conditions
-      * The temperature
-      * The humidity
-      * The wind speed
-      $("#today").text("Wind Speed: " + response.wind.speed);
-      $('#today').add('<ul>Date:' + )
-      $(".humidity").text("Humidity: " + response.main.humidity);
+     
+
+
+
+
+
+
+    
       
       // // Convert the temp to Celsius
       // var tempC = response.main.temp - 273.15;
@@ -85,44 +101,44 @@ $.ajax({
 
 
 
-$( document ).ready(function() {
-    var appID = "d00e97b068c768695d109b4ec943d226";
+// $( document ).ready(function() {
+//     var appID = "d00e97b068c768695d109b4ec943d226";
 
-    $(".query_btn").click(function(){
+//     $(".query_btn").click(function(){
 
-        var query_param = $(this).prev().val();
+//         var query_param = $(this).prev().val();
 
-        if ($(this).prev().attr("placeholder") == "City") {
-            var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
-        } else if ($(this).prev().attr("placeholder") == "Zip Code") {
-            var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
-        }
+//         if ($(this).prev().attr("placeholder") == "City") {
+//             var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
+//         } else if ($(this).prev().attr("placeholder") == "Zip Code") {
+//             var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
+//         }
 
-        $.getJSON(weather,function(json){
-            $("#city").html(json.name);
-            $("#main_weather").html(json.weather[0].main);
-            $("#description_weather").html(json.weather[0].description);
-            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
-            $("#temperature").html(json.main.temp);
-            $("#pressure").html(json.main.pressure);
-            $("#humidity").html(json.main.humidity);
-        });
-    })
+        // $.getJSON(weather,function(json){
+        //     $("#city").html(json.name);
+        //     $("#main_weather").html(json.weather[0].main);
+        //     $("#description_weather").html(json.weather[0].description);
+        //     $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+        //     $("#temperature").html(json.main.temp);
+        //     $("#pressure").html(json.main.pressure);
+        //     $("#humidity").html(json.main.humidity);
+        // });
+
 
     // Optional Code for temperature conversion
-    var fahrenheit = true;
+//     var fahrenheit = true;
 
-    $("#convertToCelsius").click(function() {
-        if (fahrenheit) {
-            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
-        }
-        fahrenheit = false;
-    });
+//     $("#convertToCelsius").click(function() {
+//         if (fahrenheit) {
+//             $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+//         }
+//         fahrenheit = false;
+//     });
 
-    $("#convertToFahrenheit").click(function() {
-        if (fahrenheit == false) {
-            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
-        }
-        fahrenheit = true;
-    });
-});
+//     $("#convertToFahrenheit").click(function() {
+//         if (fahrenheit == false) {
+//             $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
+//         }
+//         fahrenheit = true;
+//     });
+// });
